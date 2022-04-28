c7a474ed17808afecf8b0d5ed3aa9024a21e2d2b
Gage Vander Clay <gvanderclay@gmail.com>
2022-04-28 18:52:02 -0500

    Initial commit

---
+++ test/typed-cdk-config.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TypedCdkConfig from '../lib/typed-cdk-config-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/typed-cdk-config-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TypedCdkConfig.TypedCdkConfigStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
