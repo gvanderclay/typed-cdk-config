93c885317b5112681b92449d3892eeaa2b589eca
Gage Vander Clay <gvanderclay@gmail.com>
2022-04-28 19:25:02 -0500

    Add build config

--- lib/typed-cdk-config-stack.ts
+++ lib/typed-cdk-config-stack.ts
@@ -1,11 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { BuildConfig } from "./build-config";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+export type Props = StackProps & {
+  buildConfig: BuildConfig;
+};
 export class TypedCdkConfigStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
-
+    console.log(props.buildConfig.appPrefix);
     // The code that defines your stack goes here
 
     // example resource
